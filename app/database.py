71ea1f18add61f6f4abe2a32a29dd6acba4ceb51
elviskazerwa <110235165+ElvisKazerwa@users.noreply.github.com>
2024-01-11 08:21:46 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -27,7 +27,7 @@ def get_db():
 
 #while True:
  #  try:
-  #   conn = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='Kambumburi93.', cursor_factory=RealDictCursor)
+  #   conn = psycopg2.connect(host='localhost', database=' ', user='', password=' ', cursor_factory=RealDictCursor)
    #  cursor = conn.cursor()
     # print("Database connection was successfull!")
      #break
